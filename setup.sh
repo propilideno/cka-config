399a28b90ca109cbbbeae29f1607adcdcff982a6
Lucas de Almeida <105776775+propilideno@users.noreply.github.com>
2023-12-14 13:19:53 -0300

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+alias k='kubectl'
+alias kgp='kubectl get pods'
+alias kgs='kubectl get svc'
+alias kgc='kubectl get componentstatuses'
+alias kctx='kubectl config current-context'
+alias kcon='kubectl config use-context'
+alias kgc='kubectl config get-context'
+
+cat > ~/.tmux.conf <<- EOF
+# To enable mouse scroll in tmux pane
+set -g mouse on
+
+# Tmux to support colors
+set -g default-terminal "screen-256color"
+
+# Change the default prefix
+set -g prefix C-f
+unbind C-b
+
+# Use Alt + Left/Right arrows to switch tabs
+bind -n M-Left select-pane -L
+bind -n M-Right select-pane -R
+EOF
+
+cat > ~/.vimrc <<- EOF
+set et " expandtab (use space character when tab key used) *
+set ts=2 " tabstop *
+set sw=2 " shiftwidth *
+set sts=2 " softtabstop (Let backspace delete indent **
+set ai " autoindent (Indent at the same level of the previous line)
+set si " smart indent
+set hls " highlightsearch (Highlight search terms)
+set ic " ignorecase when searching in vim
+set bg=dark " Assume a dark background (better color scheme)
+set nu " show line numbers
+syntax on " syntax highlighting
+EOF
